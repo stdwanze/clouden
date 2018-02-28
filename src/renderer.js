f92422c5e65de77243fc71189d49f3c573c33fa7
Stefan Eggers <s.eggers@stuttgart.onventis.de>
2018-02-28 15:35:13 +0100

    fixed text draw

--- src/renderer.js
+++ src/renderer.js
@@ -99,9 +99,9 @@ CM.Renderer = class Renderer {
     fillText(text,x1,y1)
     {
         this.ctxt.font = "20px Ariel";
-      //  var worldX = this.translate(x1-this.viewport.x,this.canvas.width/2);
-      //  var worldY = this.translate(y1-this.viewport.y,this.canvas.height/2);
-        
+        var worldX = this.translateAndZoom(x1-this.viewport.x,this.canvas.width/2);
+        var worldY = this.translateAndZoom(y1-this.viewport.y,this.canvas.height/2);
+       
         this.ctxt.fillText(text,x1,y1);
     }
 }
